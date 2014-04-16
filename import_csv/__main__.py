ed87690c1a1892b9e670ba22176b00b28d4197a0
Adrien Katsuya Tateno <adrien.k.tateno@gmail.com>
2014-04-16 13:16:18 -0500

    Add argparse

--- import_csv/__main__.py
+++ import_csv/__main__.py
@@ -1,8 +1,17 @@
-import psycopg2
-import json
+import psycopg2, json, csv
+from argparse import ArgumentParser
 
-connection = psycopg2.connect(**json.load(open('credentials.json')))
-cursor = connection.cursor()
+parser = ArgumentParser()
+parser.add_argument('file', help='CSV file for importing.')
+parser.add_argument('-s', '--schema', action='store_true', help='Import schema.')
+parser.add_argument('-d', '--data', action='store_true', help='Import data.')
+args = parser.parse_args()
+
+print args
+
+with open('credentials.json') as credentials:
+	connection = psycopg2.connect(**json.load(credentials))
+	cursor = connection.cursor()
 
 cursor.execute("SELECT * FROM test;")
 print cursor.fetchone()
