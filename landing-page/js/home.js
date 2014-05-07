4d002218e92efb73f40b9c78c030e5598146362e
liuliu <leoliu@u.northwestern.edu>
2014-05-07 18:41:16 -0500

    highcharts
    highcharts

---
+++ landing-page/js/home.js
@@ -0,0 +1,12 @@
+
+var g1,g2,g3,g4,g5,g6;
+window.onload=function(){
+
+
+var g1=new JustGage({id:"g1",value:getRandomInt(0,100),min:0,max:100,title:"",label:"",levelColors:["#00fff6","#ff00fc","#1200ff"]});
+
+
+
+
+
+}
